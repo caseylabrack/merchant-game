0a95f6a962b4c57beca050d810c94fbb003ce810
Casey <clabrack@gmail.com>
2018-05-05 00:27:10 -0400

    fp shit

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 let d3 = require("d3")
+let {curry} = require("lodash")
 
 const Earth = "Earth",
       Venus = "Venus"
@@ -8,28 +9,30 @@ let markets =
 [
   { planet: "Earth", goods:
     [
-      { name: "twinkies", price: 100, initialQuantity: 3, currentQuantity: 3},
-      { name: "compliments", price: 1, initialQuantity: 10, currentQuantity: 10},
-      { name: "uranium", price: 800, initialQuantity: 3, currentQuantity: 3}
+      { name: "twinkies", price: 100, initialQuantity: 3, currentQuantity: 3 },
+      { name: "compliments", price: 1, initialQuantity: 10, currentQuantity: 10 },
+      { name: "uranium", price: 800, initialQuantity: 3, currentQuantity: 3 }
     ]
   },
   { planet: "Venus", goods:
     [
-      { name: "twinkies", price: 110, initialQuantity: 3, currentQuantity: 3},
-      { name: "compliments", price: 1, initialQuantity: 10, currentQuantity: 10},
-      { name: "uranium", price: 820, initialQuantity: 3, currentQuantity: 3}
+      { name: "twinkies", price: 110, initialQuantity: 3, currentQuantity: 3 },
+      { name: "compliments", price: 1, initialQuantity: 10, currentQuantity: 10 },
+      { name: "uranium", price: 820, initialQuantity: 3, currentQuantity: 3 }
     ]
   }
 ]
 
-let shipState = {
-  capacity: 10,
+let localMarket = markets.filter(z => z.planet===gameState.planet)[0]
+localMarket.goods[0].selected = true
+
+let ship = {
   money: 400,
+  capacity: 4,
   cargo: [
-    // { name: "socks", tons: 1 },
-    // { name: "zukes", tons: 2 }
-  ],
-  capacity: 4
+    { name: "socks", tons: 1 },
+    { name: "zukes", tons: 2 }
+  ]
 }
 
 let itemIndex = 0;
@@ -65,7 +68,7 @@ function updateMarket () {
   d3.select("#cargoStatus")
     .text( function (){
       let string = ""
-      shipState.cargo.forEach( function (thing) {
+      ship.cargo.forEach( function (thing) {
         string += `${thing.tons} tons of ${thing.name}, `
       })
       return string
@@ -76,7 +79,8 @@ document.addEventListener("keydown", (e) => {
 
   switch(e.key) {
     case " ":
-    console.log(canPurchase(selectItem(currentMarket(markets,gameState.planet),itemIndex),shipState))
+    purchase(getSelectedItem)
+    // console.log(canPurchase(selectItem(currentMarket(markets,gameState.planet),itemIndex),shipState))
     // markets = buyItem(itemIndex)
     break;
 
@@ -93,5 +97,23 @@ let updateSelectIndex = (e) => clampValue(e,0,markets.filter(z => z.planet===gam
 let clampValue = (e, min, max) => Math.min(Math.max(e,0),max)
 let deepCopy = d => JSON.parse(JSON.stringify(d))
 let currentMarket = (markets,planet) => deepCopy(markets.filter(z => z.planet===planet)[0])
-let selectItem = ({goods:z},index) => z[index]
-let canPurchase = ({currentQuantity:q,price:p},{money:m}) => m > p && q > 0
+let getSelectedItem = ({goods: g}) => g.filter(z => z.selected)[0]
+// let canPurchase = ({currentQuantity:q,price:p},{money:m}) => m > p && q > 0
+let getPropertyIsTrue = curry(function (arr,prop){
+  return arr.filter(z => z[prop]===true)
+})
+let marketPropertyFinder = getPropertyIsTrue(localMarket.goods)
+let canPurchase = function(ship,item){
+  return ship.capacity > playerShipTonnage("tons")
+}
+// let shipsLoad = function ({cargo:c}){
+//   c.reduce( function(sum,val){ return sum + val.tons},0)
+// }
+let sumPropertyVals = curry(function(arr,prop){
+  return arr.reduce( function(sum,val){ return sum + val[prop]},0)
+})
+let playerCargoTotals = sumPropertyVals(ship.cargo)
+
+// let marketSelector = selectWithProperty(localMarket.goods)
+
+// let selectItem = ({goods:z},index) => z[index]
